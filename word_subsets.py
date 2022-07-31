408b6aaccce8a715d9efd57d9df1019aeebf9e90
yiseul <omjl5123@catholic.ac.kr>
2022-07-31 11:38:06 +0900

    first solution

---
+++ word_subsets.py
@@ -0,0 +1,16 @@
+words1 = ["amazon","apple","facebook","google","leetcode"]
+words2 = ["e","o"]
+
+def wordSubsets(words1, words2):
+    result = []
+    for i in words1:
+        include = 1
+        for j in words2:
+            if j not in i:
+                include = 0
+                break
+        if include == 1:
+            result.append(i)
+    return result
+
+print(wordSubsets(words1, words2))
