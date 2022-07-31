f63d7b8dea239bceba3e24ad824fff0e2d760837
yiseul <omjl5123@catholic.ac.kr>
2022-07-31 20:47:18 +0900

    add another solution

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,3 +1,4 @@
+# 첫 번째 풀이 Runtime: 59ms
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         left, right = 0, len(s) - 1
@@ -17,4 +18,19 @@ class Solution:
                 else:
                     right -= 1
                     left += 1
+        return True
+
+# 두 번째 풀이 Runtime: 694ms
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        str = []
+        for char in s:
+            if char.isalnum():
+                str.append(char.lower())
+
+        # 팰린드롬 판별
+        while len(str) > 1:
+            if str.pop(0) != str.pop():
+                return False
+
         return True
