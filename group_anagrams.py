ba4242c9b811ccd51ff75d4fb3674a32a6933e5c
yiseul <omjl5123@catholic.ac.kr>
2022-08-01 23:23:07 +0900

    first solution

---
+++ group_anagrams.py
@@ -0,0 +1,7 @@
+# 파이썬 알고리즘 인터뷰 solution Runtime: 107ms
+def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    anagrams = collections.defaultdict(list)
+    for word in strs:
+        anagrams[''.join(sorted(word))].append(word)
+
+    return list(anagrams.values())
