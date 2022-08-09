7d8ff9abf9e8b0fed7e1e5965767f1f937552b43
yiseul <omjl5123@catholic.ac.kr>
2022-08-09 20:52:09 +0900

    파이썬 알고리즘 인터뷰 solution

---
+++ 배열/product_of_array_except_self.py
@@ -0,0 +1,16 @@
+# 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셉
+# 시간복잡도: O(n)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        out = []
+        p = 1
+        # 왼쪽 곱셈
+        for i in range(0, len(nums)):
+            out.append(p)
+            p *= nums[i]
+        p = 1
+        # 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈
+        for i in range(len(nums) - 1, 0 - 1, -1):
+            out[i] *= p
+            p *= nums[i]
+        return out
