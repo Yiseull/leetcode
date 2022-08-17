a27c1f7d5f1ad5a6f0b27db02fb9718cb47e4e87
yiseul <omjl5123@catholic.ac.kr>
2022-08-17 13:46:10 +0900

    first solution(hard)

--- 연결 리스트/reverse_linked_list.py
+++ 연결 리스트/reverse_linked_list.py
@@ -1,8 +1,27 @@
-# first solution
-class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        node = None
-        while head:
-            node, node.next, head = head, node, head.next
+from typing import Optional
 
-        return node
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# 파이썬 알고리즘 인터뷰 solution: 반복 구조로 노드 뒤집기
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        # 예외 처리
+        if not head or left == right:
+            return head
+
+        root = start = ListNode(None)
+        root.next = head
+        # start, end 지정
+        for _ in range(left - 1):
+            start = start.next
+        end = start.next
+
+        # 반복하면서 노드 차례대로 뒤집기
+        for _ in range(right - left):
+            tmp, start.next, end.next = start.next, end.next, end.next.next
+            start.next.next = tmp
+        return root.next
+
