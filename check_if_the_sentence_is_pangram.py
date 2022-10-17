ff0ab859c132ca0ac01ab615ad5f194778c87ce8
Yiseull <omjl5123@catholic.ac.kr>
2022-10-18 00:20:15 +0900

    Third solution

--- check_if_the_sentence_is_pangram.py
+++ check_if_the_sentence_is_pangram.py
@@ -1,12 +1,3 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
-        pangram = set()
-
-        for letter in sentence:
-            pangram.add(letter)
-
-        for i in range(97, 123):
-            if chr(i) not in pangram:
-                return False
-
-        return True
+        return len(set(sentence)) == 26
