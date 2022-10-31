1c201c1f1ae4e00bf314b4a7beb9f20622b160bb
Yiseull <omjl5123@catholic.ac.kr>
2022-10-31 16:47:58 +0900

    Add second solution

--- toeplitz_matrix.py
+++ toeplitz_matrix.py
@@ -1,8 +1,21 @@
+from typing import List
+
 class Solution:
+    # First solution
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
         for i in range(len(matrix) - 1):
             for j in range(len(matrix[0]) - 1):
                 if matrix[i][j] != matrix[i + 1][j + 1]:
                     return False
+
+        return True
+
+    # Second solution
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        length = len(matrix[0])
+
+        for i in range(len(matrix) - 1):
+            if matrix[i][:length - 1] != matrix[i + 1][1:]:
+                return False
 
         return True
